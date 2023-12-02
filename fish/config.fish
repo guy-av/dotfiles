aee0461417aaadb7a1e7aca62abc841ebfbe134d
Guy Avital <guyavital10@gmail.com>
2023-12-02 19:57:12 +0200

    fzf: Add delta and search hidden files

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fzf_configure_bindings --directory=\ct --git_status=\cs
+
+    set -gx fzf_diff_highlighter delta --paging=never --width=20
+    set -gx fzf_fd_opts --hidden --exclude=.git --no-ignore
 end
