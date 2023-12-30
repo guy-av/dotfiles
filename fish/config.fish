fcd74b101d08104effad95a2c535eec3379980b8
Guy Avital <guyavital10@gmail.com>
2023-12-30 15:41:55 +0200

    fish: Add NPM man path

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,5 @@ if status is-interactive
 
     set -gx fzf_diff_highlighter delta --paging=never --width=20
     set -gx fzf_fd_opts --hidden --exclude=.git --no-ignore
+    set -gx MANPATH $MANPATH ~/.local/share/npm-packages/share/man
 end
