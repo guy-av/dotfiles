0ecf43b0b94993e54b23c46a53ef15cbe2c69e22
Guy Avital <guyavital10@gmail.com>
2024-05-16 22:09:26 +0300

    fish: General fixes

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fzf_configure_bindings --directory=\ct --git_status=\cs
 
-    set -gx fzf_diff_highlighter delta --paging=never --width=20
-    set -gx fzf_fd_opts --hidden --exclude=.git --no-ignore
-    set -gx MANPATH $MANPATH ~/.local/share/npm-packages/share/man
+    set -x fzf_diff_highlighter delta --paging=never --width=20
+    set -x fzf_fd_opts --hidden --exclude=.git --no-ignore
+
+    set -x PAGER less -RF --mouse
+    set -x MANPAGER less -R --mouse
 end
