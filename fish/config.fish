4e1051ba8f769639efe8f266f135874a63203e64
Guy Avital <guyavital10@gmail.com>
2024-06-23 23:27:48 +0300

    fish: Tidy pagers

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,9 @@ if status is-interactive
     set -x fzf_diff_highlighter delta --paging=never --width=20
     set -x fzf_fd_opts --hidden --exclude=.git --no-ignore
 
-    set -x PAGER less -RF --mouse
-    set -x MANPAGER less -R --mouse
+    # Pagers
+    set -l BASE_PAGER less -R --mouse
+    set -x PAGER $BASE_PAGER -F
+    set -x MANPAGER $BASE_PAGER
+    set -x BAT_PAGER $PAGER
 end
